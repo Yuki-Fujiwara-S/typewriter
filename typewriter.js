1192c8a5e90342eab6b33dff00be747290986733
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-02-15 22:46:34 +0000

    added newline

--- typewriter.js
+++ typewriter.js
@@ -1,7 +1,7 @@
 const sentence = "hello there from lighthouse labs";
 let x = 0;
 
-for (const char of sentence) {
+for (const char of sentence + "\n") {
   setTimeout(() => {
     process.stdout.write(char);
   }, x) // <= 1s delay to make it noticeable. Can dial it down later.
